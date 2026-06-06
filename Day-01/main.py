d463e89e0e3a703e11ae31325c58cd73c1c358a9
Aabhas2006 <vikasaabhas1200@gmail.com>
2026-06-06 12:35:36 +0530

    Day-01

---
+++ Day-01/main.py
@@ -0,0 +1,28 @@
+driver_name = "Max Verstappen"
+team_name = "Red Bull Racing"
+car_number = 1 
+Fav_track = "Spa-Francorchamps"
+Nationality = "Dutch"
+World_Championships = 4
+
+print("Who is your favourite F1 Driver?")
+print(driver_name," is my fav driver.")
+print("Which team does he drive for?") 
+print("he drives for ",team_name)
+print("What is his car number?")
+print("His car number is ", car_number)
+print("what is your fav track?")
+print(Fav_track,"is my fav track.")
+
+print ("--- Driver's Profile ---")
+print("Driver's Name: ", driver_name)
+print("Team Name: " , team_name)
+print("Car Number:", car_number)
+print(f"Favourite Track: {Fav_track}")
+print(f"Nationality: {Nationality}")
+print(f"World Championships: {World_Championships}")
+
+
+print(driver_name)
+print(driver_name)
+print(driver_name)
