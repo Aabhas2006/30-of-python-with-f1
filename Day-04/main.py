1d7cdbdcc7110db1fc7a01deb7a7808674a56a43
Aabhas2006 <vikasaabhas1200@gmail.com>
2026-06-08 21:22:36 +0530

    Day-04

---
+++ Day-04/main.py
@@ -0,0 +1,18 @@
+drivers=["Max","Charles","Lando","Oscar","Lewis"]
+# print(drivers)
+# print(drivers[0])
+# print(drivers[1])
+
+# print("Current Race Leaders:")
+# print(drivers[0])
+position = 1
+
+for driver in drivers:
+    print(f"P{position}: {driver}")
+    position = position + 1
+
+drivers = ["Max", "Charles", "Lando"]
+drivers[1] = "Lewis"
+print(drivers)
+
+#learned about lists, indexing, updating list items, len() variable, looping through lists and counters 
