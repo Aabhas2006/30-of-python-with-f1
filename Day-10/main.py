274ee76f92e36d34cace64e92f75db70062b148e
Aabhas2006 <vikasaabhas1200@gmail.com>
2026-06-16 11:11:54 +0530

    Day-10

---
+++ Day-10/main.py
@@ -0,0 +1,25 @@
+Driver = {
+    "name": "Max Verstappen",
+    "team": "Oracle RedBull Racing",
+    "number": 1,
+    "Nationality": "Dutch",
+    "WDC won": 4
+}
+
+Driver["number"] = 3
+Driver["WDC points"] = 55
+
+print('Driver Analysis')
+print(Driver["name"])
+print(Driver["team"])
+print(Driver["number"])
+print(Driver["Nationality"])
+print(Driver["WDC won"])
+print(Driver["WDC points"])
+
+# Todays's Learning :
+
+# 1. Dictionary is used to store related information together.
+# 2. Dictionary uses keys to access it .Lists uses index/position to access it.
+# 3. We can add and alter dictionary using a simple command :
+#    Dict_name[add or altering key name] = value
